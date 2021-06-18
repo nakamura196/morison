c1b903e279a384ea3ce753cf4b523f55085235f0
nakamura196 <na.kamura.1263@gmail.com>
2021-06-18 12:29:39 +0900

    fix: bug

--- src/000_batch.sh
+++
@@ -1,8 +0,0 @@
-# python 01_ItemGenerator.py
-# python 02_ManifestGenerator.py
-python 11_PropertiesGenerator.py
-python 12_ItemSetsGenerator.py
-python 13_ResourceTemplatesGenerator.py
-python 14_SitesGenerator.py
-# python 21_MediaGenerator.py
-# python 31_FileDownloader.py
